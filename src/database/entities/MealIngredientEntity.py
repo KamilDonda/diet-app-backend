6d4ea758132eaf10b7f991c00956fa7d2ea3df46
Kamil Donda <dondakamil@gmail.com>
2022-01-08 21:33:53 +0100

    Update MealIngredientEntity

--- src/database/entities/MealIngredientEntity.py
+++ src/database/entities/MealIngredientEntity.py
@@ -1,16 +1,16 @@
 class MealIngredientEntity:
-    def __init__(self, id, meal_id, ingredient_id, description, amount):
+    def __init__(self, id, ingredient_id, meal_id, description, amount):
         self.id = id
-        self.meal_id = meal_id
         self.ingredient_id = ingredient_id
+        self.meal_id = meal_id
         self.description = description
         self.amount = amount
 
     def serialize(self):
         return {
             'id': self.id,
-            'meal_id': self.meal_id,
             'ingredient_id': self.ingredient_id,
+            'meal_id': self.meal_id,
             'description': self.description,
             'amount': self.amount
         }
